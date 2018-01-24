dc9649baa24abae977e7e42d7d6544d66e46cb48
edwin <edwin@LAPTOP-P4L0AJ9U>
2018-01-23 21:48:24 -0500

    comentado

--- JUEGO/src/Juego/Main.java
+++ JUEGO/src/Juego/Main.java
@@ -5,7 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		VentanaInicio v=new VentanaInicio();
-		//*VentanaPrincipal vp=new VentanaPrincipal(); 
+		VentanaPrincipal vp=new VentanaPrincipal(); 
 		//secundaria s=new secundaria();
 	
 	}
